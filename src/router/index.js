e21d5bb93005cfefa2e8de51b8850ea491a82e3f
djwGod <djwilo@126.com>
2021-09-22 20:20:46 +0800

    项目基于vant优化完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,41 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+// 重写路由push方法,阻止重复点击报错
+const originalPush = VueRouter.prototype.push;
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err);
+};
 
+// 重写路由replace方法,阻止重复点击报错
+const originalReplace = VueRouter.prototype.replace;
+VueRouter.prototype.replace = function replace(location) {
+  return originalReplace.call(this, location).catch(err => err);
+};
+
+Vue.use(VueRouter);
+
+// 正常路由加载,会将所有路由的js跟css合并到一个文件
+import Home from "../views/home/Home";
+import Category from "../views/category/Category";
+import Cart from "../views/cart/Cart";
+import Profile from "../views/profile/Profile";
+import ProductDetail from "../views/detail/ProductDetail";
+
+// 路由配置
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+  { path: "/", name: "home", component: Home },
+  { path: "/category", name: "category", component: Category },
+  { path: "/cart", name: "cart", component: Cart },
+  { path: "/profile", name: "profile", component: Profile },
+  { path: "/detail/:id", name: "detail", component: ProductDetail },
+  { path: "*", redirect: "/" }
+];
 
 const router = new VueRouter({
+  mode: "history",
+  base: "/",
   routes
-})
+});
 
-export default router
+export default router;
